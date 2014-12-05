a7469c04e6f7e9e3d153a0b3dc433f164b89f6d8
Sharif Olorin <sio@tesser.org>
2014-12-05 18:58:55 +1100

    OpenStack UUIDs are RFC4122 v4 UUIDs

--- lib/System/OpenStack/Types.hs
+++ lib/System/OpenStack/Types.hs
@@ -3,9 +3,13 @@ module System.OpenStack.Types
     OpenStackConfig
 ) where
 
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as S
 import           Data.Text
+import           Data.UUID
 import           Network.URI
 
+-- | The base configuration needed to connect to an OpenStack API.
 data OpenStackConfig = OpenStackConfig {
       _authURL  :: URI
     , _username :: Text
